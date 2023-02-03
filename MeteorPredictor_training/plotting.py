9805517c8fd7a2e7f436d20738aa3cd8b4a8012e
Yeonho Yoo <yyho1994@naver.com>
2023-02-04 01:00:43 +0900

    Update plotting.py

--- MeteorPredictor_training/plotting.py
+++ MeteorPredictor_training/plotting.py
@@ -1,5 +1,22 @@
 # Author: Laura Kulowski
 
+'''
+#MIT License
+#
+#Copyright (c) 2020 Laura Kulowski
+#
+#Permission is hereby granted, free of charge, to any person obtaining a copy
+#of this software and associated documentation files (the "Software"), to deal
+#in the Software without restriction, including without limitation the rights
+#to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+#copies of the Software, and to permit persons to whom the Software is
+#furnished to do so, subject to the following conditions:
+#
+#The above copyright notice and this permission notice shall be included in all
+#copies or substantial portions of the Software.
+#
+'''
+
 import numpy as np
 import matplotlib.pyplot as plt
 import torch
