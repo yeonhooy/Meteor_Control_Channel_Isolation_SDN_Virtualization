ca185410719ba3d557c5f6dec0652c2a5d69e592
yeonhooy <yyho1994@naver.com>
2023-02-08 19:48:55 +0900

    edit script for meteor

--- Meteor/scripts/ovx.sh
+++ Meteor/scripts/ovx.sh
@@ -9,16 +9,16 @@ JVM_OPTS="-Xms512m -Xmx2g"
 #JVM_OPTS="$JVM_OPTS -javaagent:${OVXHOME}/lib/jacocoagent.jar=dumponexit=true,output=file,destfile=${OVXHOME}/target/jacoco.exec"
 JVM_OPTS="$JVM_OPTS -XX:+TieredCompilation"
 JVM_OPTS="$JVM_OPTS -XX:+UseCompressedOops"
-JVM_OPTS="$JVM_OPTS -XX:+UseConcMarkSweepGC -XX:+AggressiveOpts -XX:+UseFastAccessorMethods"
+#JVM_OPTS="$JVM_OPTS -XX:+UseConcMarkSweepGC -XX:+AggressiveOpts -XX:+UseFastAccessorMethods"
 JVM_OPTS="$JVM_OPTS -XX:MaxInlineSize=8192 -XX:FreqInlineSize=8192" 
-JVM_OPTS="$JVM_OPTS -XX:CompileThreshold=1500 -XX:PreBlockSpin=8" 
+#JVM_OPTS="$JVM_OPTS -XX:CompileThreshold=1500 -XX:PreBlockSpin=8" 
 
 if [ ! -e ${OVX_JAR} ]; then
   cd ${OVXHOME}
-  echo "Packaging OVX for you..."
+  echo "Packaging Meteor(OVX) for you..."
   mvn package > /dev/null
   cd -
 fi
 
-echo "Starting OpenVirteX..."
+echo "Starting Meteor..."
 java ${JVM_OPTS} -Dlog4j.configurationFile=${OVXHOME}/config/log4j2.xml -Djavax.net.ssl.keyStore=${OVXHOME}/config/sslStore -jar ${OVX_JAR} $@
